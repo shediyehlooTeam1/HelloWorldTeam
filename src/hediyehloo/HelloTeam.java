28f67518905c28b4fa6bfbcb60de1d85fb825dfe
sdjibian <sdjibian@student.tgm.ac.at>
2018-02-13 13:05:16 +0100

    Sakos Commit

--- src/hediyehloo/HelloTeam.java
+++ src/hediyehloo/HelloTeam.java
@@ -2,7 +2,7 @@ package hediyehloo;
 
 public class HelloTeam {
 	public static void hallovonsam() {
-		System.out.println("Schönes Sommersemester");
+		System.out.println("Schönes Sommersemester von Sako");
 	}
 	public static void main(String [] args) {
 		hallovonsam();
