d8bb9cfad3412f7b96c6e6c3a28e68a2b3e44733
shediyehloo <shediyehloo@student.tgm.ac.at>
2018-02-13 11:41:29 +0100

    first commmit

---
+++ src/hediyehloo/HelloTeam.java
@@ -0,0 +1,10 @@
+package hediyehloo;
+
+public class HelloTeam {
+	public static void hallovonsam() {
+		System.out.println("Schönes Sommersemester");
+	}
+	public static void main(String [] args) {
+		hallovonsam();
+	}
+}
