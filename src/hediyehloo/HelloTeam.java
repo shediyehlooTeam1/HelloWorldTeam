39299d1a3baa19a4e926e87f4878f30e22430d77
shediyehloo <shediyehloo@student.tgm.ac.at>
2018-02-13 12:12:51 +0100

    second

--- src/hediyehloo/HelloTeam.java
+++ src/hediyehloo/HelloTeam.java
@@ -2,7 +2,7 @@ package hediyehloo;
 
 public class HelloTeam {
 	public static void hallovonsam() {
-		System.out.println("Schönes Sommersemester");
+		System.out.println("Schönes Sommersemesterhjjh");
 	}
 	public static void main(String [] args) {
 		hallovonsam();
