301796f4a054edaf0fe410ce8fac8d7855368eb6
sdjibian <sdjibian@student.tgm.ac.at>
2018-02-13 12:24:05 +0100

    Erster Sam commit.

--- src/hediyehloo/HelloTeam.java
+++ src/hediyehloo/HelloTeam.java
@@ -2,7 +2,7 @@ package hediyehloo;
 
 public class HelloTeam {
 	public static void hallovonsam() {
-		System.out.println("Schönes Sommersemesterhjjh");
+		System.out.println("Schönes Sommersemester");
 	}
 	public static void main(String [] args) {
 		hallovonsam();
