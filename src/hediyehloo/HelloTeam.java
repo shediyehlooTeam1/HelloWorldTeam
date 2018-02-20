9e4e280b886f6fb6e0edf2bf277a1eddb8847aa6
SakoDjibian7 <35727584+SakoDjibian7@users.noreply.github.com>
2018-02-20 11:50:37 +0100

    Update HelloTeam.java
    Commit

--- src/hediyehloo/HelloTeam.java
+++ src/hediyehloo/HelloTeam.java
@@ -2,7 +2,7 @@ package hediyehloo;
 
 public class HelloTeam {
 	public static void hallovonsam() {
-		System.out.println("Schönes Sommersemester von Sako");
+		System.out.println("SchÃ¶nes Sommersemester von Sako Djibian");
 	}
 	public static void main(String [] args) {
 		hallovonsam();
